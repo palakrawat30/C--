119a2024969fe5df060d335ba42fd533d29d9f2e
palakrawat30 <nokatakamepalak@gmail.com>
2025-10-24 23:54:14 +0530

    added variables

---
+++ variables.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5,a=2;
+    char y;
+    string z;
+    float b=2.5;
+    z="Palak";
+    y='a';
+    //x=6;
+    // cout<<x+a;
+    // x+=2;
+    // cout<<x;
+    // cout<<y;
+    
+// ARITHEMATIC OPERATOR
+    /*cout<<x+a<<endl;
+    cout<<x-a<<endl;
+    cout<<x*a<<endl;
+    cout<<x/a<<endl;
+    cout<<b/a<<endl;
+    cout<<x%a<<endl;*/
+
+// INCREMENT DECREMENT OPERATOR
+    // cout<<x<<endl;
+    // cout<<x++<<endl;
+    // cout<<x<<endl;
+    // cout<<++x<<endl;
+    // cout<<x<<endl;
+    // x++;    //Post increment
+    // cout<<x<<endl;
+    // x--;
+    // cout<<x<<endl;
+    // ++x;    //Pre increment
+    // cout<<x<<endl;
+    // --x;
+    // cout<<x<<endl;
+
+// Area of a circle
+    /*int r;
+    float area,pie=3.14;
+    r=2;
+    area=pie*(r*r);
+    cout<<area<<endl;*/
+
+// Simple Interest
+    /*int p=100,r=3,t=2;
+    float si;
+    si=(p*r*t)/100;
+    cout<<si<<endl;*/
+
+// Taking user input
+    /*int c;
+    string d;
+    cin>>d;
+    // cout<<c*c<<endl;
+    cout<<d<<endl;*/
+
+// TYPE CASTING (converting to different data types)
+    /*int e;
+    cin>>e;
+    float f= (float)e;
+    cout<<f/2;*/
+
+//Cheacking ASCII Value of char's
+    /*char e;
+    cin>>e;
+    cout<<(int)e;*/
+
+//Cheacking ASCII Value to char's
+    /*int e;
+    cin>>e;
+    cout<<(char)e;*/
+    
+    cout<<7/22*(3.14+2)*3/5;
+    
+    return 0;
+}
