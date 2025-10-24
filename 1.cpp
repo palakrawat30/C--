e0255ea611877732c191b77ee22ce2c852aea4ee
palakrawat30 <nokatakamepalak@gmail.com>
2025-10-24 23:51:46 +0530

    added 1

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World";
+    cout<<endl; // Enter next line OR end of line
+    cout<<"Hello World\n";
+    cout<<"Hello World 0"<<endl<<"Hello World 1\n";
+    cout<<4<<endl<<4+3;
+    return 0;
+}
